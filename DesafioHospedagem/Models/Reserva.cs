c1fa7e1c230749788f6a5d7841e930476141147c
joao.borba <joao.borba@dclogisticsbrasil.com>
2023-09-25 22:17:48 -0300

    Desafio Hospedagem

---
+++ DesafioHospedagem/Models/Reserva.cs
@@ -0,0 +1,54 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            if (hospedes.Any() && hospedes.Count <= Suite.Capacidade)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                if (hospedes.Count > Suite.Capacidade)
+                {
+                    throw new Exception("Capacidade superior a quantidade permitida na suite!");
+                }
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            if (DiasReservados >= 10)
+            {
+                decimal desconto = Convert.ToDecimal(valor * 10 / 100);
+                return valor - desconto;
+            }
+
+            return valor;
+        }
+    }
+}
